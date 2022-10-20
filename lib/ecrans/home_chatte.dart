51dbb640494adc5974c6b281c3d2ff942d94320c
Assoko Kévin <110037168+Kevinevrad@users.noreply.github.com>
2022-10-20 20:33:16 +0000

    new modifs

--- lib/ecrans/home_chatte.dart
+++ lib/ecrans/home_chatte.dart
@@ -3,6 +3,7 @@ import 'package:chatapp/data_theme.dart';
 import 'package:chatapp/ecrans/chat_page.dart';
 import 'package:chatapp/models/message_model.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../Widgets/mt_tab_bar.dart';
 import '../Widgets/recent_chats.dart';
 import '../models/user_model.dart';
@@ -50,19 +51,61 @@ class _HomeChatState extends State<HomeChat> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.person),
-        ),
         title: Text(
           'Chatty',
           style: MyTheme.kAppTitle,
         ),
         actions: [
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {},
-          )
+          PopupMenuButton<MenuItem>(
+              padding: EdgeInsets.all(0),
+              color: Theme.of(context).appBarTheme.backgroundColor,
+              itemBuilder: (context) => [
+                    PopupMenuItem(
+                      height: 70,
+                      value: MenuItem.item1,
+                      child: Container(
+                        width: 200,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(
+                              Icons.account_circle,
+                              color: Colors.black,
+                              size: 40,
+                            ),
+                            Text(
+                              'My account',
+                              style: GoogleFonts.poppins(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    PopupMenuItem(
+                      height: 70,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.logout_outlined,
+                            color: Colors.black,
+                            size: 40,
+                          ),
+                          SizedBox(
+                            width: 18,
+                          ),
+                          Text(
+                            'Log Out',
+                            style: GoogleFonts.poppins(fontSize: 18),
+                          ),
+                          SizedBox(
+                            width: 72,
+                          )
+                        ],
+                      ),
+                      value: MenuItem.item2,
+                    ),
+                  ])
         ],
       ),
       backgroundColor: MyTheme.kPrimaryColor,
@@ -108,4 +151,9 @@ class _HomeChatState extends State<HomeChat> with TickerProviderStateMixin {
       ),
     );
   }
+}
+
+enum MenuItem {
+  item1,
+  item2,
 }
