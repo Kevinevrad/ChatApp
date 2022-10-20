b5029fde013d914fe937df352b6319a79281451f
Assoko Kévin <110037168+Kevinevrad@users.noreply.github.com>
2022-10-20 14:15:32 +0000

    pubspec changes

--- lib/ecrans/onbording_page.dart
+++ lib/ecrans/onbording_page.dart
@@ -3,7 +3,6 @@ import 'package:chatapp/widgets_perso/bouttons.dart';
 import 'package:chatapp/widgets_perso/cartes_onbordingpade.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../utils/theme/theme.dart';
 import '../variables/var_constantes/image_var.dart';
